06b7408288eb4af49cb25dfaa57fce310cb1ac54
Jeremy Bell <jb@jeremybell.com>
2016-11-03 18:56:55 -0400

    removed getForecast

--- bot.js
+++ bot.js
@@ -91,17 +91,3 @@ if (require.main === module) {
   const client = getWit();
   client.interactive();
 }
-
-getForecast({context, entities}) {
-  return new Promise(function(resolve, reject) {
-    var location = firstEntityValue(entities, "location")
-    if (location) {
-      context.forecast = 'sunny in ' + location; // we should call a weather API here
-      delete context.missingLocation;
-    } else {
-      context.missingLocation = true;
-      delete context.forecast;
-    }
-    return resolve(context);
-  });
-},
