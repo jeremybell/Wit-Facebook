4a50c3da208f311e879429d454f881a237f227e0
Jeremy Bell <jb@jeremybell.com>
2016-11-08 09:50:32 -0500

    Added getForecast logic

--- bot.js
+++ bot.js
@@ -91,3 +91,19 @@ if (require.main === module) {
   const client = getWit();
   client.interactive();
 }
+
+// Adding Bot Business Logic
+
+getForecast({context, entities}) {
+  return new Promise(function(resolve, reject) {
+    var location = firstEntityValue(entities, "location")
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return resolve(context);
+  });
+},
